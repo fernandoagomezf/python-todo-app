9256e139ba3d0d3ffa2892664cc924021e34c8d2
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-14 21:39:20 -0600

    feat: agregar manejo de acciones y registro de controladores en la aplicación web

--- src/application/controllers.py
+++ src/application/controllers.py
@@ -1,12 +1,50 @@
 from flask import Flask
 from flask import render_template
 from flask import flash
-from typing import Any
-from typing import Callable
+from typing import Any, Callable
 
 class Controller():    
+    _actions: dict[str, Callable[[dict[str, Any]], Any]]
     def __init__(self):
-        self._routes = self.build_routes()
+        self._actions = { }
+
+    def map(self, action: str, func: Callable[[dict[str, Any]], Any]) -> None:
+        if action is None or action == "":
+            raise ValueError("Action cannot be null or empty")
+        if func is None:
+            raise ValueError("Function cannot be null")
+        self._actions[action] = func
+    
+    def invoke(self, action: str, data:dict[str, Any]) -> Any:
+        if action is None or action == "":
+            raise ValueError("Action cannot be null or empty")
+        data = data or { }
+        func = self._actions.get(action)
+        if func is None:
+            raise ValueError(f"Action '{action}' not found")
+        return func(data)
+
+    def message(self, message: str) -> None:
+        flash(message, "info")
+
+    def alert(self, message: str) -> None:
+        flash(message, "warning")
+
+    def error(self, message: str) -> None:
+        flash(message, "danger")
+
+
+class HomeController(Controller):
+    def __init__(self):
+        super().__init__()
+        self.map("get_index", self.index)
+        self.map("get_about", self.about)
+
+    def index(self, _) -> Any:
+        return render_template("index.html", title="Home")
+    
+    def about(self, _) -> Any:
+        return render_template("about.html", title="About")
 
     
     
--- src/application/webapp.py
+++ src/application/webapp.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from infrastructure.database import Database
+from application.controllers import Controller
 
 class Config():
     _basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,11 +12,13 @@ class Config():
 class WebApp():
     _engine: Flask
     _db: Database
+    _map: dict[str, dict[str, Controller]]
     
     def __init__(self, name:str):
         self._engine = Flask(name)
         self._engine.config.from_object(Config)
         self._db = Database(self._engine, Config.SQLALCHEMY_DATABASE_URI)
+        self._map = { }
         self._db.create()
 
     def get_engine(self) -> Flask:
@@ -27,6 +30,16 @@ class WebApp():
     def start(self):
         self._engine.run(debug=Config.DEBUG)
 
+    def register(self, section:str, controller: Controller) -> None:
+        if section is None or section == "":
+            raise ValueError("Section cannot be null or empty")
+        if controller is None:
+            raise ValueError("Controller cannot be null")
+        self._map[section] = controller
 
-
-    
+    def route(self, section: str, action: str, data: dict[str, any] = {}) -> any:
+        data = data or {}
+        ctrl = self._map.get(section)
+        if ctrl is None:
+            raise ValueError(f"Section '{section}' not found")
+        return ctrl.invoke(action, data)
