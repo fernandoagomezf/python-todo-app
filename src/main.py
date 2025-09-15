2f652c3e8eb1c392a0f9a528d2d3e8de1f7a5042
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-14 21:39:27 -0600

    feat: refactorizar rutas y controlador de la página de inicio

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from flask import render_template
+from uuid import UUID
 from application.webapp import WebApp
-import uuid
+from application.controllers import HomeController
 
 webapp = WebApp(__name__)
+webapp.register("home", HomeController())
 
-@webapp.get_engine().route('/')
-@webapp.get_engine().route('/index')
-def index():
-    return render_template("index.html", title="Home")
+@webapp.get_engine().route("/")
+@webapp.get_engine().route("/index")
+@webapp.get_engine().route("/home/index")
+def home_index():
+    return webapp.route("home", "get_index")
+
+@webapp.get_engine().route("/about")
+def home_about():
+    return webapp.route("home", "get_about")
 
 @webapp.get_engine().route('/task/<uuid:task_id>')
-def task(task_id: uuid.UUID):
-    return render_template("task.html", title="Task Details", task_id=str(task_id))
+def task_detail(id: UUID):
+    return webapp.route("task", "detail", {"id": id})
 
 webapp.start()
