fcac411ba1cde0a86c366c3c3b89fd46d6c80904
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-09-14 18:14:07 -0600

    fix: corregir la URI de la base de datos en la configuración de la aplicación web

--- src/application/webapp.py
+++ src/application/webapp.py
@@ -5,7 +5,7 @@ from infrastructure.database import Database
 class Config():
     _basedir = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(_basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(_basedir, 'todo.db')
     DEBUG = True
 
 class WebApp():
